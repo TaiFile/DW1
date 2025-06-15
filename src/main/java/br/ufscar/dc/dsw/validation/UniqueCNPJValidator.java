caf43cf6bb22bc7a2d2b258dca3f0cd917e7f224
Eduardo Malagutti <eduardo.malagutti@catijr.com.br>
2025-06-15 12:33:51 -0300

    refactor: project folder

---
+++ src/main/java/br/ufscar/dc/dsw/validation/UniqueCNPJValidator.java
@@ -0,0 +1,27 @@
+package br.ufscar.dc.dsw.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UniqueCNPJValidator implements ConstraintValidator<UniqueCNPJ, String> {
+
+    // @Autowired
+    // private IEditoraDAO dao;
+
+    // @Override
+    // public boolean isValid(String CNPJ, ConstraintValidatorContext context) {
+    //     if (dao != null) {
+    //         Editora editora = dao.findByCNPJ(CNPJ);
+    //         return editora == null;
+    //     } else {
+    //         Durante a execução da classe LivrariaMvcApplication
+    //         não há injeção de dependência
+    //        return true;
+    //     }
+
+    // }
+}
