43a0c75f5502ea30f228146a5593c79bee7abbe2
Eduardo Malagutti <eduardo.malagutti@catijr.com.br>
2025-06-16 14:51:38 -0300

    feat: offer entity

---
+++ src/main/java/br/ufscar/dc/dsw/domain/enums/OfferStatus.java
@@ -0,0 +1,7 @@
+package br.ufscar.dc.dsw.domain.enums;
+
+public enum OfferStatus {
+    OPEN,
+    ACCEPTED,
+    REJECTED
+}
---
+++ src/main/java/br/ufscar/dc/dsw/domain/enums/PaymentMethodEnum.java
@@ -0,0 +1,7 @@
+package br.ufscar.dc.dsw.domain.enums;
+
+public enum PaymentMethodEnum {
+    CREDIT_CARD,
+    DEBIT_CARD,
+    MONEY
+}
