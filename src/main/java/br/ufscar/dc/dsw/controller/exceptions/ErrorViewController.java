479fbfcaab2019847ea12db7a857e87d708e4f24
Eduardo Malagutti <eduardo.malagutti@catijr.com.br>
2025-06-20 10:49:47 -0300

    feat: exception handling

--- src/main/java/br/ufscar/dc/dsw/controller/exceptions/ErrorViewController.java
+++ src/main/java/br/ufscar/dc/dsw/controller/exceptions/ErrorViewController.java
@@ -1,4 +1,4 @@
-package br.ufscar.dc.dsw.controller;
+package br.ufscar.dc.dsw.controller.exceptions;
 
 import java.util.Map;
 
@@ -9,6 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * Controller de visualização de erro personalizado que trata respostas de erro HTTP na aplicação.
+ * Este componente é usado pelo Spring Security e Spring Web, por baixo dos panos, para fornecer páginas de erro personalizadas
+ * para diferentes códigos de status HTTP (403, 404, e outros).
+ */
 @Component
 public class ErrorViewController implements ErrorViewResolver {
 
---
+++ src/main/java/br/ufscar/dc/dsw/controller/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,61 @@
+package br.ufscar.dc.dsw.controller.exceptions;
+
+import br.ufscar.dc.dsw.exceptions.BadRequestException;
+import br.ufscar.dc.dsw.exceptions.ResourceNotFoundException;
+import br.ufscar.dc.dsw.exceptions.StorageException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * ControllerAdvice responsável por tratar exceções globais da aplicação, lançadas explícitamente no código.
+ * Redireciona exceções personalizadas para uma página de erro amigável.
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 4xx
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ModelAndView handleResourceNotFoundException(ResourceNotFoundException e, HttpServletRequest request) {
+        ModelAndView model = new ModelAndView("error");
+        model.addObject("status", HttpStatus.NOT_FOUND.value());
+        model.addObject("error", "exception.not_found");
+        model.addObject("message", e.getMessage());
+        model.addObject("path", request.getRequestURI());
+        return model;
+    }
+
+
+    @ExceptionHandler(BadRequestException.class)
+    public ModelAndView handleBadRequestException(BadRequestException e, HttpServletRequest request) {
+        ModelAndView model = new ModelAndView("error");
+        model.addObject("status", HttpStatus.BAD_REQUEST.value());
+        model.addObject("error", "exception.bad_request");
+        model.addObject("message", e.getMessage());
+        model.addObject("path", request.getRequestURI());
+        return model;
+    }
+
+    // 5xx
+    @ExceptionHandler(StorageException.class)
+    public ModelAndView handleStorageException(StorageException e, HttpServletRequest request) {
+        ModelAndView model = new ModelAndView("error");
+        model.addObject("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        model.addObject("error", "exception.storage");
+        model.addObject("message", e.getMessage());
+        model.addObject("path", request.getRequestURI());
+        return model;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleGenericException(Exception e, HttpServletRequest request) {
+        ModelAndView model = new ModelAndView("error");
+        model.addObject("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        model.addObject("error", "exception.internal_server_error");
+        model.addObject("message", "Ocorreu um erro inesperado no servidor");
+        model.addObject("path", request.getRequestURI());
+        return model;
+    }
+}
