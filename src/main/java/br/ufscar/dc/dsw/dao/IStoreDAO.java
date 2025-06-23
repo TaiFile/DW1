1ef0123712c5913a7eec2808007455167238dec8
JntReguim <jonatareg@gmail.com>
2025-06-23 16:07:29 -0300

    fix:registerVehicle

--- src/main/java/br/ufscar/dc/dsw/dao/IStoreDAO.java
+++ src/main/java/br/ufscar/dc/dsw/dao/IStoreDAO.java
@@ -3,7 +3,6 @@ package br.ufscar.dc.dsw.dao;
 import br.ufscar.dc.dsw.domain.Store;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.repository.ListCrudRepository;
-import java.util.List;
 import java.util.Optional;
 
 public interface IStoreDAO extends ListCrudRepository<Store, Long> {
