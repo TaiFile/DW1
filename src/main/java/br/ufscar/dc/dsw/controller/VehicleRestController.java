1587066ef0133083c1e8cbd50057a4a691f5023d
Eduardo Malagutti <eduardosm2004@gmail.com>
2025-07-11 23:46:22 -0300

    fix: VehicleRestController

--- src/main/java/br/ufscar/dc/dsw/controller/VehicleRestController.java
+++ src/main/java/br/ufscar/dc/dsw/controller/VehicleRestController.java
@@ -1,6 +1,8 @@
 package br.ufscar.dc.dsw.controller;
 
+import br.ufscar.dc.dsw.domain.Store;
 import br.ufscar.dc.dsw.domain.Vehicle;
+import br.ufscar.dc.dsw.service.spec.IStoreService;
 import br.ufscar.dc.dsw.service.spec.IVehicleService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,25 +20,8 @@ public class VehicleRestController {
     @Autowired
     private IVehicleService vehicleService;
 
-    @GetMapping
-    public ResponseEntity<List<Vehicle>> listVehicles() {
-
-        List<Vehicle> vehicles = vehicleService.findAllAvailable();
-        if (vehicles.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(vehicles);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Vehicle> listByID(@PathVariable("id") Long id) {
-
-        Vehicle vehicle = vehicleService.findById(id);
-        if (vehicle == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(vehicle);
-    }
+    @Autowired
+    private IStoreService storeService;
 
     @GetMapping("/models/{model}")
     public ResponseEntity<List<Vehicle>> listByModel(@PathVariable("model") String model) {
@@ -58,14 +43,20 @@ public class VehicleRestController {
         return ResponseEntity.ok(vehicles);
     }
 
-    @PostMapping
+    @PostMapping("/stores/{storeId}")
     @ResponseBody
     public ResponseEntity<Vehicle> create(@Valid @RequestBody Vehicle vehicle,
                                           BindingResult result) {
-
         if (result.hasErrors()) {
             return ResponseEntity.badRequest().body(null);
         }
+
+        Store store = storeService.findById(vehicle.getStore().getId());
+        if (store == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        vehicle.setStore(store);
 
         Vehicle savedVehicle = vehicleService.save(vehicle);
         return ResponseEntity.ok(savedVehicle);
